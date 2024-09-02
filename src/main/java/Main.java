f53ad8401a15957e6c26a02870597acadb25c525
Alexis Hernández bolivar <169068450+alexisjhb@users.noreply.github.com>
2024-09-02 12:08:51 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+  int numero = sc.nextInt();
+    System.out.println("O numero digitado foi:");
+    System.out.println(numero);
+  if(numero > 0){
+System.out.println("O numero e positivo");
+  }
+if(numero < 0){
+System.out.println("O numero e negativo");
+  
+}
+if(numero == 0);{
+System.out.println("O numero e igual a zero");
+  
+  
+}    
+
+
+
+    
+    sc.close();
+    
+  
+  
+  
+  }
+
+}
